af31fadffa3c5e3f681d4776b5d4049c92c8889e
Biren Kothari <biren.k@prominentpixel.com>
2023-09-15 19:13:14 +0530

    Adding Practice Files for React

--- to-do-list/src/components/NewTodo.tsx
+++ to-do-list/src/components/NewTodo.tsx
@@ -1,31 +1,45 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+import "./NewTodo.css";
 
 type NewTodoProps = {
 	onAddTodo: (text: string) => void;
 };
 
 const NewTodo: React.FC<NewTodoProps> = (props) => {
-	const task = useRef<HTMLInputElement>(null);
+	const [isValid, setIsValid] = useState(true);
+	const [newTodo, setNewTodo] = useState("");
 
 	const submitHandler = (event: React.FormEvent) => {
 		event.preventDefault();
-		props.onAddTodo(task.current!.value);
-		task.current!.value = "";
+
+		if (newTodo.trim().length === 0) {
+			setIsValid(false);
+			return;
+		}
+
+		props.onAddTodo(newTodo);
+		setNewTodo("");
+	};
+
+	const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.value.trim().length > 0) {
+			setIsValid(true);
+		}
+		setNewTodo(event.target.value);
 	};
 
 	return (
-		<form
-			action=""
-			onSubmit={submitHandler}
-		>
-			<label htmlFor="todoInput">Add New Task : </label>
-			<input
-				type="text"
-				id="todoInput"
-				ref={task}
-			/>
-			<button type="submit">Add Task</button>
-		</form>
+		<div className={`form-control ${!isValid ? "invalid" : ""}`}>
+			<form onSubmit={submitHandler}>
+				<label htmlFor="todoInput">Add New Task : </label>
+				<input
+					type="text"
+					id="todoInput"
+					onChange={inputChangeHandler}
+				/>
+				<button type="submit">Add Task</button>
+			</form>
+		</div>
 	);
 };
 
