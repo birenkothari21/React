8eb0e6c0acc8843281da1da222c63598d16aee8f
Biren Kothari <biren.k@prominentpixel.com>
2023-09-25 19:38:00 +0530

    Adding practice files

---
+++ demo-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Redux Counter</h1>
+			<Counter></Counter>
+		</div>
+	);
+}
+
+export default App;
